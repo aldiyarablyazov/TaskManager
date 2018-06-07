1997adcb6f94b613827a11e7dccd95f96794ac97
aldiyar <aldiyarablyazov@gmail.com>
2018-06-07 21:02:50 +0200

    Deleted InterruptTimer class. Was not working as I hoped.

--- src/taskmanager/AccountController.java
+++ src/taskmanager/AccountController.java
@@ -1,23 +1,11 @@
 package taskmanager;
 
-import java.util.Timer;
-
 public class AccountController {
     public static boolean isValidAccount() {
 
-        Timer timer = new Timer(true);
-        InterruptTimer interruptTimer =
-                new InterruptTimer(Thread.currentThread());
-        timer.schedule(interruptTimer, 2);
-        try {
-            DatabaseController dataControl = new DatabaseController();
-            dataControl.handleConnect();
-            throw new InterruptedException();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        } finally {
-            timer.cancel();
-            return true;
-        }
+        DatabaseController dataControl = new DatabaseController();
+        dataControl.handleConnect();
+        return true;
+
     }
 }
